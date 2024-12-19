d978a4cef92c08f001617cd82d4fabe845352014
Your Name <your-email@example.com>
2024-12-19 21:00:00 +0530

    Added plugins still autopairs

---
+++ lua/plugins/nvim-tree.lua
@@ -0,0 +1,17 @@
+return {
+  "nvim-tree/nvim-tree.lua",
+  version = "*",
+  lazy = false,
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
+  config = function()
+    local nvimtree = require("nvim-tree")
+
+    vim.g.loaded_netrw = 1
+    vim.g.loaded_netrwPlugin = 1
+    require("nvim-tree").setup {}
+
+    vim.keymap.set("n", "<C-n>", ":NvimTreeFindFileToggle<CR>")
+  end,
+}
