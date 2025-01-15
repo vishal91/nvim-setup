eda60f8d1e4a6f7da3d30a673344a149aeab4322
Vishal Dandge <vishaldandge1@gmail.com>
2025-01-15 17:19:03 +0530

    Changes for debugger

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,10 +1,27 @@
 return {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000, 
-
-    config = function()
-        vim.cmd.colorscheme "catppuccin"
-    end
+         "catppuccin/nvim",
+         name = "catppuccin",
+         lazy = false,
+         priority = 1000,
+         config = function()
+             require("catppuccin").setup({
+                 -- flavour = "latte",
+                 color_overrides = {
+                     mocha = {
+                            -- base = "#181825", -- very dark purple
+                            -- mantle = "#181825"
+                            -- base = "#1e1e2e", -- light dark purple
+                            -- mantle = "#1e1e2e"
+                          base = "#1b1f20", -- gray black <- THIS
+                          mantle = "#1b1f20" --           <- THIS
+                          -- base = "#111111",  -- dark blackl
+                          -- mantle = "#111111" -- dark blackl
+                          -- base = "#090d17",
+                          -- mantle = "#090d17"
+                     }
+                 }
+             })
+             vim.cmd.colorscheme "catppuccin"
+         end
 }
-
+ 
